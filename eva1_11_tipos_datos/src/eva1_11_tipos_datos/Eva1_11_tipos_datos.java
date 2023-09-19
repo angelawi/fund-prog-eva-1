040442fb959f001edcc8131ed65e6bfab61407ce
angelawi <143562997+angelawi@users.noreply.github.com>
2023-09-19 17:49:52 -0600

    Add files via upload

---
+++ eva1_11_tipos_datos/src/eva1_11_tipos_datos/Eva1_11_tipos_datos.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_11_tipos_datos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class Eva1_11_tipos_datos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte califa; 
+        califa = 127; 
+        // califa = 200; no se puede en byte
+        //califa = -128; 
+        //califa = -200; no se puede en byte
+        //califa = 127 + 1;
+        Scanner input = new Scanner(System.in); 
+        System.out.println("Sumar valor;");
+        byte captu = input.nextByte();
+        System.out.println(califa + captu);
+        
+        
+        int valor;
+        valor = Integer.MAX_VALUE;
+        System.out.println(valor + 1);
+        valor = Integer.MIN_VALUE;
+        System.out.println(valor);
+        
+        
+        char caracter = 'A'; 
+        System.out.println(caracter);
+        System.out.println(caracter + 0); 
+     
+    }
+    
+}
